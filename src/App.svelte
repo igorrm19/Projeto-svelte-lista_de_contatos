bd0332339342feb067a5244cfbcd319a3b0fc42e
igorrm19 <git@stackblitz.com>
2024-11-08 21:18:23 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import {contador} from "./stores.js"
+  import "./app.css"
+  import Pesquisar from "./lib/pesquisar.svelte"
+</script>
+
+
+<main>
+
+  <div id="cabesalho">
+     <h1>Liata de tarefas</h1>
+     <i id="contador" class="fa-solid fa-users">{$contador}</i>
+  </div>
+
+  <div id="pesquisa">
+    <Pesquisar />
+  </div>
+ 
+</main>
+
+
+
+
+
+<style>
+  #cabesalho{
+    display: flex;
+    gap: 2rem;
+    justify-content: center;
+    align-items: center;
+  }
+
+#contador{
+  color:green;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#pesquisa{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
---
+++ src/lib/pesquisar.svelte
@@ -0,0 +1,66 @@
+
+<script>
+ import {contador} from '../stores.js'
+ 
+  let lista = [
+    {id: 1, nome: "Igor", email: "igormachado14@gmail", status:  true},
+    {id: 2, nome: "Briza", email: "brizamachado14@gmail", status: false},
+    {id: 3, nome: "Tierre", email: "tierremachado14@gmail", status: true}
+  ];
+
+  contador.set(lista.length) 
+</script>
+
+
+<div id="container">
+  <div class="input-group">
+    <div class="input-icon">
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+
+    <input id="sereach"
+           type="text"
+           class="input-field"
+           placeholder="pesquisar">
+  </div>
+
+
+  <ul class="items">
+   {#each lista as newlista}
+    <li>
+
+      <h3>Contato {newlista.id},  {newlista.nome},  {newlista.email}</h3>
+
+      {#if newlista.status == true}
+        <i style class="fa-solid fa-tree status-ativo"></i>
+      {/if}
+
+       {#if newlista.status === false}
+         <i class="fa-solid fa-tree  status-inativo"></i>
+       {/if}
+
+    </li>
+   {/each}
+  </ul>
+
+</div>
+
+<style>
+  li{
+    display: flex;
+    gap: 2rem;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .status-ativo{
+    color: green;
+  }
+
+  .status-inativo{
+    color: red;
+  }
+</style>
+
+
